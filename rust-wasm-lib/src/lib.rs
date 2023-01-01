535cc10a1cffd9a6c26fc330310d4cf75fa5f67b
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-01-01 17:10:41 +0100

    Added makeMove functionality and changed handleClick

--- rust-wasm-lib/src/lib.rs
+++ rust-wasm-lib/src/lib.rs
@@ -1,12 +1,17 @@
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, vec};
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
-// #[wasm_bindgen]
-// extern "C" {
-//     #[wasm_bindgen(typescript_type = "IFigure")]
-//     pub type IFigure;
-// }
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    console_error_panic_hook::set_once();
+}
+
+#[wasm_bindgen(start)]
+pub fn run() {
+    init_panic_hook();
+}
 
 #[derive(Default, Clone, Serialize, Deserialize)]
 pub struct IFigure {
@@ -17,10 +22,13 @@ pub struct IFigure {
 #[wasm_bindgen]
 pub fn possible_moves(i: u8, figure_map: JsValue) -> Result<JsValue, JsError> {
     let figure_map: HashMap<u8, IFigure> = serde_wasm_bindgen::from_value(figure_map)?;
-    let fig:IFigure;
+    let fig: IFigure;
     match figure_map.get(&i) {
         Some(f) => fig = f.clone(),
-        None => {fig = IFigure::default(); return Err(JsError::new("Error loading fig"))}
+        None => {
+            fig = IFigure::default();
+            return Err(JsError::new("Error loading fig"));
+        }
     }
 
     let mut poss_moves: Vec<u8> = vec![];
@@ -36,12 +44,12 @@ pub fn possible_moves(i: u8, figure_map: JsValue) -> Result<JsValue, JsError> {
             },
             _ => {
                 match figure_map.get(&(i + 11)) {
-                    Some(_) => poss_moves.push(i + 11),
-                    None => (),
+                    Some(_) => (),
+                    None => poss_moves.push(i + 11),
                 }
                 match figure_map.get(&(i + 9)) {
-                    Some(_) => poss_moves.push(i + 9),
-                    None => (),
+                    Some(_) => (),
+                    None => poss_moves.push(i + 9),
                 }
             }
         }
@@ -57,12 +65,12 @@ pub fn possible_moves(i: u8, figure_map: JsValue) -> Result<JsValue, JsError> {
             },
             _ => {
                 match figure_map.get(&(i - 11)) {
-                    Some(_) => poss_moves.push(i - 11),
-                    None => (),
+                    Some(_) => (),
+                    None => poss_moves.push(i - 11),
                 }
                 match figure_map.get(&(i - 9)) {
-                    Some(_) => poss_moves.push(i - 9),
-                    None => (),
+                    Some(_) => (),
+                    None => poss_moves.push(i - 9),
                 }
             }
         }
--- src/App.tsx
+++ src/App.tsx
@@ -1,130 +1,146 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import init, {possible_moves} from './pkg';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import init, { possible_moves } from "./pkg";
 
-
-interface IFigure { 
+interface IFigure {
   color: "black" | "white";
   kind: "man" | "king";
 }
 
-function Figure(props: IFigure)
-{
-  return (
-    <div className="circle" style={{ backgroundColor: props.color}}/>
-  );
+function Figure(props: IFigure) {
+  return <div className="circle" style={{ backgroundColor: props.color }} />;
 }
 
 interface ISquareProps {
-  onClick: React.MouseEventHandler<HTMLButtonElement>; 
   value: IFigure | null;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
   color: string;
 }
 
-function Square(props: ISquareProps) 
-{
+function Square(props: ISquareProps) {
   return (
-    <button 
-    className="square"
-    style={{ backgroundColor: props.color}} 
-    onClick={props.onClick}>
-      {props.value && <Figure color={props.value.color} kind={props.value.kind} />}
+    <button
+      className="square"
+      style={{ backgroundColor: props.color }}
+      onClick={props.onClick}
+    >
+      {props.value && (
+        <Figure color={props.value.color} kind={props.value.kind} />
+      )}
     </button>
   );
 }
 
 interface IBoardProps {
   figure: Map<number, IFigure>;
-  possibleMoves: number[] | null;
-  onClick: (squareNo:number, squareValue:IFigure|null) => void;
+  possibleMoves: number[];
+  onClick: (squareNo: number, squareValue: IFigure | null) => void;
 }
 
 function Board(props: IBoardProps) {
   const board = [];
-  for(let i = 0; i < 10; i++){
+  for (let i = 0; i < 10; i++) {
     const squareRows = [];
-    for(let j = 0; j < 10; j++){
-      const squareNo = i*10+j;
-      const backgroundColor = 
-      isOnDarkDiag(squareNo) ? "#693e3e" : "#ffd6ae";
+    for (let j = 0; j < 10; j++) {
+      const squareNo = i * 10 + j;
+      const backgroundColor = isOnDarkDiag(squareNo) ? "#693e3e" : "#ffd6ae";
       const figure = props.figure.get(squareNo) ?? null;
       squareRows.push(
         <Square
-        value={figure}
-        onClick={() => props.onClick(squareNo, figure)}
-        color={props.possibleMoves && props.possibleMoves[squareNo] ? 'sandybrown' : backgroundColor}
-      />
+          key={squareNo}
+          value={figure}
+          onClick={() => props.onClick(squareNo, figure)}
+          color={
+            props.possibleMoves.includes(squareNo)
+              ? "sandybrown"
+              : backgroundColor
+          }
+        />
       );
     }
-    board.push(<div className="board-row">{squareRows}</div>)
+    board.push(
+      <div key={i} className="board-row">
+        {squareRows}
+      </div>
+    );
   }
-
-  return (
-    <div>
-      {board}
-    </div>
-  );
+  return <div>{board}</div>;
 }
 
 export default function Game() {
   const [figureMap, setFigureMap] = useState<Map<number, IFigure>>(
     getInitialFiguresState()
-    );
-  const [seletedSquare, setSeletedSquare] = useState<number | null>(null);
+  );
+  const [selectedFigureNo, setSelectedFigureNo] = useState<number | null>(null);
   const [whiteIsNext, setWhiteIsNext] = useState<boolean>(true);
-  const [possibleMoves, setPossibleMoves] = useState<number[]|null>(null);
+  const [possibleMoves, setPossibleMoves] = useState<number[]>([]);
 
   useEffect(() => {
     init().then(() => {
-      const possibleMoves = seletedSquare && possible_moves(seletedSquare, figureMap);
-      setPossibleMoves(possibleMoves)
-    })
-  });
-  const handleClick = (i:number, figure:IFigure|null) => {
-    const newFigureMap = new Map<number, IFigure>(figureMap);
-    let newSelectedSquare = null;
-    if (figureMap.get(i)) {
-      newSelectedSquare = (seletedSquare && seletedSquare === i) ? null : i;
-      console.log(seletedSquare);
-      console.log(seletedSquare && figureMap.get(seletedSquare));
-      console.log(possibleMoves);
+      const possibleMoves = selectedFigureNo
+        ? possible_moves(selectedFigureNo, figureMap)
+        : [];
+      setPossibleMoves(possibleMoves);
+    });
+  }, [figureMap, selectedFigureNo]);
+
+  const makeMove = (
+    sourceSquareNo: number,
+    targetSquareNo: number,
+    figureMap: Map<number, IFigure>
+  ) => {
+    const sourceSqareFigure = figureMap.get(sourceSquareNo);
+    if (sourceSqareFigure) {
+      const newFigureMap = new Map<number, IFigure>(figureMap);
+      newFigureMap.delete(sourceSquareNo);
+      newFigureMap.set(targetSquareNo, sourceSqareFigure);
+      setFigureMap(newFigureMap);
+      setSelectedFigureNo(null);
+      setWhiteIsNext(!whiteIsNext);
     }
+  };
 
-    setSeletedSquare(newSelectedSquare);
-    setFigureMap(newFigureMap);
-  }
-
-
-  
+  const handleClick = (clickedSquareNo: number, figure: IFigure | null) => {
+    if (
+      figureMap.get(clickedSquareNo)?.color ===
+      (whiteIsNext ? "white" : "black")
+    ) {
+      setSelectedFigureNo(
+        selectedFigureNo && selectedFigureNo === clickedSquareNo
+          ? null
+          : clickedSquareNo
+      );
+    } else if (selectedFigureNo && possibleMoves.includes(clickedSquareNo)) {
+      makeMove(selectedFigureNo, clickedSquareNo, figureMap);
+    }
+  };
   return (
     <div className="game">
       <div className="game-board">
-      <div className="status">{'Next player: ' + (whiteIsNext ? 'White' : 'Black')}</div>
-        <Board 
-        figure={figureMap}
-        possibleMoves={possibleMoves}
-        onClick={handleClick}
+        <div className="status">
+          {"Next player: " + (whiteIsNext ? "White" : "Black")}
+        </div>
+        <Board
+          figure={figureMap}
+          possibleMoves={possibleMoves}
+          onClick={handleClick}
         />
       </div>
     </div>
   );
 }
 
-const isOnDarkDiag = (i:number) : boolean => {
-  return [1, 3, 5, 7, 9].includes(Math.abs(i%10-Math.floor(i/10))%11)
-}
+const isOnDarkDiag = (i: number): boolean => {
+  return [1, 3, 5, 7, 9].includes(Math.abs((i % 10) - Math.floor(i / 10)) % 11);
+};
 
-const getInitialFiguresState = () : Map<number, IFigure> => {
+const getInitialFiguresState = (): Map<number, IFigure> => {
   const map = new Map<number, IFigure>();
-  for (let i=0; i<100; ++i) {
+  for (let i = 0; i < 100; ++i) {
     if (isOnDarkDiag(i)) {
-      if (i < 30)
-        map.set(i, {color:'black', kind:'man'});
-      if (i >= 70)
-        map.set(i, {color:'white', kind:'man'});
+      if (i < 30) map.set(i, { color: "black", kind: "man" });
+      if (i >= 70) map.set(i, { color: "white", kind: "man" });
     }
   }
   return map;
-} 
-
-
+};
